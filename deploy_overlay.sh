2ef9b1b3ac3f545a312d4b66c90b282f4407745a
Ilja Livenson <ilja.livenson@gmail.com>
2013-05-09 18:11:19 +0100

    Cleanup

--- deploy_overlay.sh
+++ deploy_overlay.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#I hate this overlay method, but it seems to be the way things are done.
 #Note that files with the same name are overwritten so be careful to prefix
 #files with name of module or similar scheme.
 
