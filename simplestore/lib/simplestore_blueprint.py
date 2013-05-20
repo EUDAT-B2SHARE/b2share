a418a63749710ef82dbd2022d8e09810cdf1fc92
Adrian Mouat <adrian.mouat@gmail.com>
2013-05-20 15:18:32 +0100

    Pull file upload stuff into separate file.

--- simplestore/lib/simplestore_blueprint.py
+++ simplestore/lib/simplestore_blueprint.py
@@ -20,22 +20,21 @@
 """SimpleStore Flask Blueprint"""
 import os
 import uuid
-import shutil
 import time
-from glob import iglob
-from flask import (render_template, request, jsonify, current_app)
-from werkzeug.utils import secure_filename
+from tempfile import mkstemp
+from flask import render_template, request, current_app
 from flask.ext.wtf import Form
+from wtforms.ext.sqlalchemy.orm import model_form
 from invenio.webinterface_handler_flask_utils import _, InvenioBlueprint
 from invenio.config import CFG_SITE_SECRET_KEY
 from invenio.sqlalchemyutils import db
-from invenio.simplestore_model.model import Submission, SubmissionMetadata, LinguisticsMetadata
+from invenio.simplestore_model.model import (Submission, SubmissionMetadata,
+                                             LinguisticsMetadata)
 from invenio.bibtask import task_low_level_submission
-from wtforms.ext.sqlalchemy.orm import model_form
-from invenio.simplestore_model.HTML5ModelConverter import HTML5ModelConverter
 from invenio.bibfield_jsonreader import JsonReader
-from tempfile import mkstemp
 from invenio.config import CFG_TMPSHAREDDIR
+from invenio.simplestore_model.HTML5ModelConverter import HTML5ModelConverter
+import invenio.simplestore_file_uploads as uploads
 
 blueprint = InvenioBlueprint('simplestore', __name__,
                              url_prefix='/simplestore',
@@ -44,8 +43,6 @@ blueprint = InvenioBlueprint('simplestore', __name__,
                                           'simplestore.deposit', 2)],
                              breadcrumbs=[(_('SimpleStore'),
                                           'simplestore.deposit')])
-
-CFG_SIMPLESTORE_UPLOAD_FOLDER = '/opt/invenio/var/tmp/simplestore_uploads'
 
 
 @blueprint.route('/')
@@ -63,119 +60,28 @@ class FormWithKey(Form):
 
 
 @blueprint.route('/upload/<uid>', methods=['POST'])
-def plupload(uid):
-    """ The file is split into chunks on the client-side
-        and reformed on the server-side
-    """
-    if request.method == 'POST':
-        try:
-            chunks = request.form['chunks']
-            chunk = request.form['chunk']
-        except KeyError:
-            chunks = None
-            pass
-        name = request.form['name']
-        current_chunk = request.files['file']
-
-        try:
-            filename = secure_filename(name) + "_" + chunk
-        except UnboundLocalError:
-            filename = secure_filename(name)
-
-        if not os.path.exists(CFG_SIMPLESTORE_UPLOAD_FOLDER):
-            os.makedirs(CFG_SIMPLESTORE_UPLOAD_FOLDER)
-
-        # webdeposit also adds userid and deptype folders
-        CFG_USER_SIMPLESTORE_FOLDER = os.path.join(
-            CFG_SIMPLESTORE_UPLOAD_FOLDER, uid)
-
-        if not os.path.exists(CFG_USER_SIMPLESTORE_FOLDER):
-            os.makedirs(CFG_USER_SIMPLESTORE_FOLDER)
-
-        # Save the chunk
-        current_chunk.save(os.path.join(CFG_USER_SIMPLESTORE_FOLDER, filename))
-
-        unique_filename = ""
-
-        if chunks is None:  # file is a single chunk
-            unique_filename = str(uuid.uuid1()) + filename
-            old_path = os.path.join(CFG_USER_SIMPLESTORE_FOLDER, filename)
-            file_path = os.path.join(CFG_USER_SIMPLESTORE_FOLDER,
-                                     unique_filename)
-            os.rename(old_path, file_path)  # Rename the chunk
-            #size = os.path.getsize(file_path)
-            #file_metadata = dict(name=name, file=file_path, size=size)
-            #draft_field_list_add(current_user.get_id(), uuid,
-            #                     "files", file_metadata)
-        elif int(chunk) == int(chunks) - 1:
-            '''All chunks have been uploaded!
-                start merging the chunks'''
-            filename = secure_filename(name)
-            chunk_files = []
-            for chunk_file in iglob(os.path.join(CFG_USER_SIMPLESTORE_FOLDER,
-                                                 filename + '_*')):
-                chunk_files.append(chunk_file)
-
-            # Sort files in numerical order
-            chunk_files.sort(key=lambda x: int(x.split("_")[-1]))
-
-            unique_filename = str(uuid.uuid1()) + filename
-            file_path = os.path.join(CFG_USER_SIMPLESTORE_FOLDER,
-                                     unique_filename)
-            destination = open(file_path, 'wb')
-            for chunk in chunk_files:
-                shutil.copyfileobj(open(chunk, 'rb'), destination)
-                os.remove(chunk)
-            destination.close()
-            #size = os.path.getsize(file_path)
-            #file_metadata = dict(name=name, file=file_path, size=size)
-            #draft_field_list_add(current_user.get_id(), uuid,
-            #                     "files", file_metadata)
-    return unique_filename
+def upload(uid):
+    return uploads.upload(request, uid)
 
 
-@blueprint.route('/plupload_delete/<id>', methods=['GET', 'POST'])
-def plupload_delete(id):
-    if request.method == 'POST':
-        #filename = request.form['filename']
-        files = os.listdir(os.path.join(CFG_SIMPLESTORE_UPLOAD_FOLDER, id))
-        # delete all for minute
-        for f in files:
-            os.remove(f)
-            result = "File " + f['name'] + " Deleted"
-            break
-    return result
+@blueprint.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
+    return uploads.delete(request, id)
 
 
-#don't think we need this
-@blueprint.route('/plupload_get_file/<uuid>', methods=['GET'])
-def plupload_get_file(uuid):
-#    filename = request.args.get('filename')
-#    tmp = ""
-#    files = draft_field_get(current_user.get_id(), uuid, "files")
-#    for f in files:
-#        tmp += f['file'].split('/')[-1] + '<br><br>'
-#        if filename == f['file'].split('/')[-1]:
-#            return send_file(f['file'],
-#                             attachment_filename=f['name'],
-#                             as_attachment=True)
-#
-#    return "filename: " + filename + '<br>' + tmp
-    return ""
+@blueprint.route('/get_file/<uuid>', methods=['GET'])
+def get_file(uuid):
+    return uploads.get_file(uuid)
 
 
 @blueprint.route('/check_status/<uuid>/', methods=['GET', 'POST'])
 def check_status(uuid):
-    # setting to status to 1 causes a reload. I'm not sure when we want to do
-    # this. Possibly when upload complete?
-    return jsonify({"status": 0})
+    return uploads.check_status(uuid)
 
 
 @blueprint.route('/check_status/', methods=['GET', 'POST'])
 def check_status_noarg():
-    # setting to status to 1 causes a reload. I'm not sure when we want to do
-    # this. Possibly when upload complete?
-    return jsonify({"status": 0})
+    return uploads.check_status_noarg()
 
 
 @blueprint.route('/addmeta', methods=['POST'])
@@ -214,7 +120,7 @@ def addmeta():
     #else:
     #   print meta_form.errors
 
-    files = os.listdir(os.path.join(CFG_SIMPLESTORE_UPLOAD_FOLDER, request.form['uuid']))
+    files = os.listdir(os.path.join(uploads.CFG_SIMPLESTORE_UPLOAD_FOLDER, request.form['uuid']))
 
     return render_template(
         'simplestore-addmeta.html',
