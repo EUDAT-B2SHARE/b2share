34f16d4d2e33ac3f3a01b0dc783458d851a9bfd0
Adrian Mouat <adrian.mouat@gmail.com>
2013-06-04 09:39:01 +0100

    Temporary hack to create table on ss-dev.

--- simplestore/lib/simplestore_deposit_handler.py
+++ simplestore/lib/simplestore_deposit_handler.py
@@ -59,7 +59,7 @@ def deposit(request):
 
         #Uncomment the following line if there are errors regarding db tables
         #not being present. Hacky solution for minute.
-        #db.create_all()
+        db.create_all()
 
         db.session.add(sub)
         db.session.commit()
