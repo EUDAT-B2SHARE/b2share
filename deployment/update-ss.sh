4a118743e035730774920701ef8368d4adb18160
Ilja Livenson <ilja.livenson@gmail.com>
2013-07-15 15:49:28 +0300

    Updated default demo branch to master

--- deployment/update-ss.sh
+++ deployment/update-ss.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # update to the latest version
-(cd /home/admin/src/simplestore && sudo -u admin git pull origin demo)
+(cd /home/admin/src/simplestore && sudo -u admin git pull origin master)
 (cd /home/admin/src/simplestore && sudo -u admin pip install -r requirements.txt)
 
 # deploy
