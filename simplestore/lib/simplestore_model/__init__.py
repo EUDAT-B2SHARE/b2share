7eb85867725d96e5b7f3c9f620ec158a6f7ebab2
Adrian Mouat <adrian.mouat@gmail.com>
2013-07-16 11:56:30 +0100

    Support setting metadata domains in config.
     - by setting the CFG_SIMPLESTORE_DOMAINS variable in
       invenio-local.conf, you can now control the domains that are
       listed during deposition e.g.
    
       CFG_SIMPLESTORE_DOMAINS = ecology, drihm
    
       will cause only the ecology and drihm domains to appear. Note that
       you need to make sure the metadataclasses are avaiable for your
       domains and you will need to update invenio on any changes to the conf
       file.

--- simplestore/lib/simplestore_model/__init__.py
+++ simplestore/lib/simplestore_model/__init__.py
@@ -4,10 +4,19 @@ import pkgutil
 
 # might well be a better way to do this
 metadata_classes = {SubmissionMetadata.domain.lower(): SubmissionMetadata}
+try:
+    from invenio.config import CFG_SIMPLESTORE_DOMAINS
+    configured_domains = [d.strip().lower() for d in
+                          CFG_SIMPLESTORE_DOMAINS.split(',')]
+except ImportError:
+    configured_domains = None
+
 
 pck = metadata
 prefix = pck.__name__ + '.'
 for imp, modname, ispkg in pkgutil.iter_modules(pck.__path__, prefix):
     # not sure what fromlist does...
     mod = __import__(modname, fromlist="dummy")
-    metadata_classes[mod.domain.lower()] = _create_metadata_class(mod)
+    domain_name = mod.domain.lower()
+    if not configured_domains or domain_name in configured_domains:
+        metadata_classes[domain_name] = _create_metadata_class(mod)
