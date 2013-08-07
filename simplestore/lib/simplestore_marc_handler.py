442f446481df0524feaa077ad93bbf6ac12932e7
Huw Morris <huw.morris@stfc.ac.uk>
2013-08-07 11:12:58 +0100

    Return the error code from the EPIC server. Use CFG_FAIL_ON_MISSING_PID to determine whether to abort with an error, or simply to write the error to flask.log.

--- simplestore/lib/simplestore_marc_handler.py
+++ simplestore/lib/simplestore_marc_handler.py
@@ -2,7 +2,6 @@ import os
 from datetime import datetime
 from invenio.dbquery import run_sql
 from invenio.bibrecord import record_add_field, record_xml_output
-<<<<<<< HEAD
 from invenio.config import CFG_SIMPLESTORE_UPLOAD_FOLDER
 from invenio.config import CFG_SITE_SECURE_URL
 from invenio.config import CFG_SITE_NAME
@@ -10,7 +9,7 @@ from invenio.config import (CFG_SIMPLESTORE_UPLOAD_FOLDER, CFG_SITE_NAME,
                             CFG_SITE_SECURE_URL)
 from invenio.simplestore_model.model import SubmissionMetadata
 from invenio.simplestore_epic import createHandle
-
+from flask import current_app
 from werkzeug.exceptions import HTTPException
 from invenio.simplestore_model import metadata_classes
 
@@ -159,7 +158,20 @@ def create_marc(form, sub_id, email):
         record_add_field(rec, '024', ind1='7',
                          subfields = [('2', 'PID'), ('a', pid)])
     except HTTPException as e:
-        raise e
+        # If CFG_FAIL_ON_MISSING_PID is not found in invenio-local.conf,
+        # default is to assume True
+        try:
+            from config import CFG_FAIL_ON_MISSING_PID
+            fail = bool(CFG_FAIL_ON_MISSING_PID)
+        except:
+            fail = True
+
+        if (fail):
+            raise e
+        else:
+            current_app.logger.error(
+                "Unable to obtain PID from EPIC server {0} {1}: {2}".
+                format(e.code, e.name, e.get_description()))
 
     marc = record_xml_output(rec)
 
