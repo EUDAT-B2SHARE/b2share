858cb5f88161b92261595a9b9179b2298ac9bb9e
Huw Morris <huw.morris@stfc.ac.uk>
2013-08-08 09:10:47 +0100

    Further code style improvements

--- simplestore/lib/simplestore_marc_handler.py
+++ simplestore/lib/simplestore_marc_handler.py
@@ -141,20 +141,19 @@ def create_marc(form, sub_id, email):
                          subfields = [('2', 'PID'), ('a', pid)])
     except HTTPException as e:
         # If CFG_FAIL_ON_MISSING_PID is not found in invenio-local.conf,
-        # default is to assume True
+        # default is to assume False
         try:
             from config import CFG_FAIL_ON_MISSING_PID
             fail = bool(CFG_FAIL_ON_MISSING_PID)
-        except:    
-            fail = True
+        except ImportError:    
+            fail = False
                      
-        if (fail):
-            raise e
-        else:
-            current_app.logger.error(
+        current_app.logger.error(
                 "Unable to obtain PID from EPIC server {0} {1}: {2}".
-                format(e.code, e.name, e.get_description()))
-            
+                format(e.code, e.name, e.get_description()))            
+        if fail:
+            raise e
+         
     marc = record_xml_output(rec)
 
     return recid, marc
