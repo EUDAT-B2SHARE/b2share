9b7ddf2df2bb1b3df4b307d21c1bfd3b1e37d5d4
Emanuel Dima <emanueldima@gmail.com>
2014-01-15 11:47:28 +0100

    improved update-help script

--- deployment/update-help.sh
+++ deployment/update-help.sh
@@ -3,13 +3,13 @@
 INVENIO_DIR=/opt/invenio
 
 git config --global http.sslVerify false
-cd simplestore.wiki
+cd ../simplestore.wiki
 git pull
 
-cp B2SHARE-About.md $INVENIO_DIR/etc/templates/b2share-about.markdown
-cp B2SHARE-FAQ.md $INVENIO_DIR/etc/templates/b2share-faq.markdown
-cp B2SHARE-TOU.md $INVENIO_DIR/etc/templates/b2share-tou.markdown
-cp User-Documentation.md $INVENIO_DIR/etc/templates/b2share-guide.markdown
+cp -v B2SHARE-About.md $INVENIO_DIR/etc/templates/b2share-about.markdown
+cp -v B2SHARE-FAQ.md $INVENIO_DIR/etc/templates/b2share-faq.markdown
+cp -v B2SHARE-TOU.md $INVENIO_DIR/etc/templates/b2share-tou.markdown
+cp -v User-Documentation.md $INVENIO_DIR/etc/templates/b2share-guide.markdown
 
-cp -rf img/* /opt/invenio/var/img/
+cp -vrf img/* /opt/invenio/var/img/
 
