e3d8a5140e8884e3caa1abcc82b255c22fe9db7b
Emanuel Dima <emanueldima@gmail.com>
2014-01-20 00:52:05 +0100

    added euon specific fields and logo

--- simplestore/lib/simplestore_model/metadata/euon_metadata_config.py
+++ simplestore/lib/simplestore_model/metadata/euon_metadata_config.py
@@ -6,26 +6,22 @@ from datetime import date
 domain = "euon"
 display_name = "EUON"
 table_name = 'euon'
-image = 'euon_icon.jpg'
+image = 'EUON-logo.png'
 kind = 'project'
 
 
 # note that fields will need more stuff like validators later
 fields = [
-          {'name': 'ref_date',
-           'col_type': db.Date(),
-           'display_text': 'Reference date',
-           'required': True,
-           'default': date.today()},
-          {'name': 'reference_system',
+          {'name': 'hasDomain',
            'col_type': db.String(256),
-           'display_text': 'Reference System',
+           'display_text': 'Ontology Domain',
            'required': True},
-         ]
-
-
-
-# hasDomain	a category that describes the ontology, from a pre-defined list of categories (e.g., Anatomy, names of specific organisms, Diseases, etc.)	We have to create a list of domains: proposed take list of bioportal and widen it.
-# hasOntologyLanguage	the language in which the ontology was developed	We have to setup a list of formal languages
-# usedOntologyEngineeringTool	the tool that was used to develop the ontology	If someone wants to look on it
-# creationDate, modificationDate
+          {'name': 'hasOntologyLanguage',
+           'col_type': db.String(256),
+           'display_text': 'Ontology Language',
+           'required': True},
+          {'name': 'usedOntologyEngineeringTool',
+           'col_type': db.String(256),
+           'display_text': 'Ontology Engineering Tool ',
+           'required': True},
+         ]         
