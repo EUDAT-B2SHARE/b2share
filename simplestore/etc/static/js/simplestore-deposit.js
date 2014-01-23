02ac8a7a74725fc0684082ae6c18dedfa7c8493e
llehtine <lassi.lehtinen@csc.fi>
2014-01-23 14:06:57 +0200

    Update simplestore-deposit.js

--- simplestore/etc/static/js/simplestore-deposit.js
+++ simplestore/etc/static/js/simplestore-deposit.js
@@ -91,7 +91,7 @@ function simplestore_init_plupload(selector, url, delete_url, get_file_url) {
                 runtimes : 'html5',
                 url : url,
                 max_file_size : '2048mb',
-                chunk_size : '1mb',
+                chunk_size : '20mb',
                 //unique_names : true,
                 browse_button : 'pickfiles',
                 drop_element : 'filebox'
