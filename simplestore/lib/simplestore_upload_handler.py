94ee10584ea753fb340005c16b4544e380ed2363
Emanuel Dima <emanueldima@gmail.com>
2014-02-12 15:29:54 +0100

    fixed #114

--- simplestore/lib/simplestore_upload_handler.py
+++ simplestore/lib/simplestore_upload_handler.py
@@ -31,7 +31,7 @@ import pickle
 
 from werkzeug.utils import secure_filename
 
-from flask import send_file
+from flask import send_file, current_app
 
 from invenio.config import CFG_SIMPLESTORE_UPLOAD_FOLDER
 
@@ -46,20 +46,17 @@ def upload(request, sub_id):
         if sub_id.startswith('/'):
             # malformed uuid, can lead to data escalation, raise an error
             raise Exception('UUID is malformed')
+
         try:
             chunks = request.form['chunks']
             chunk = request.form['chunk']
         except KeyError:
             chunks = None
-            pass
+            chunk = 0
+
         # generate a unique name to be used for submission
         name = request.form['name']
         current_chunk = request.files['file']
-
-        try:
-            filename = secure_filename(name) + "_" + chunk
-        except UnboundLocalError:
-            filename = secure_filename(name)
 
         if not os.path.exists(CFG_SIMPLESTORE_UPLOAD_FOLDER):
             os.makedirs(CFG_SIMPLESTORE_UPLOAD_FOLDER)
@@ -70,45 +67,50 @@ def upload(request, sub_id):
         if not os.path.exists(upload_dir):
             os.makedirs(upload_dir)
 
+        import hashlib
+        hasher = hashlib.md5()
+        hasher.update(name)
+        md5 = hasher.hexdigest()
+
         # Save the chunk
+        safename = secure_filename(name)
+        filename = safename + "_" + md5 + "_" + chunk
         path_to_save = os.path.join(upload_dir, filename)
         current_chunk.save(path_to_save)
+        current_app.logger.warning("    chunk %s" % (filename,))
 
-        if chunks is None:  # file is a single chunk
-            unique_filename = str(new_uuid())
-            old_path = os.path.join(upload_dir, filename)
-            file_path = os.path.join(upload_dir,
-                                     unique_filename)
-            os.rename(old_path, file_path)  # Rename the chunk
-            size = os.path.getsize(file_path)
-            file_metadata = dict(name=name, file=file_path, size=size)
-        elif (chunks is not None) and (int(chunk) == int(chunks) - 1):
-            '''All chunks have been uploaded!
-                start merging the chunks'''
-            filename = secure_filename(name)
-            chunk_files = []
-            for chunk_file in iglob(os.path.join(upload_dir,
-                                                 filename + '_*')):
-                chunk_files.append(chunk_file)
-
-            # Sort files in numerical order
-            chunk_files.sort(key=lambda x: int(x.split("_")[-1]))
-
-            file_uuid = str(new_uuid())
-            file_path = os.path.join(upload_dir, file_uuid)
-            metadata_file_path = os.path.join(upload_dir, 'metadata_' + file_uuid + filename)
-            destination = open(file_path, 'wb')
-            for chunk in chunk_files:
-                shutil.copyfileobj(open(chunk, 'rb'), destination)
-                os.remove(chunk)
-            destination.close()
-            size = os.path.getsize(file_path)
-            file_metadata = dict(name=name, file=file_path, size=size)
-            # create a metadata-<uuid>-<safe-file-name> file to store pickled metadata
-            pickle.dump(file_metadata, open(metadata_file_path, 'wb'))
-
+        if (chunks is None) or (int(chunk) == int(chunks) - 1):
+            '''All chunks have been uploaded! Start merging the chunks!'''
+            merge_chunks_and_create_metadata(upload_dir, name, md5, safename)
     return filename
 
+def merge_chunks_and_create_metadata(upload_dir, name, md5, safename):
+    root, ext = os.path.splitext(safename)
+    if ext in ['.gz', '.bz2']:
+        ext = os.path.splitext(root)[1] + ext
+
+    file_uuid_name = str(new_uuid()) + ext
+    file_path = os.path.join(upload_dir, file_uuid_name)
+
+    chunk_files = []
+    for chunk_file in iglob(os.path.join(upload_dir, safename + "_" + md5 + '_*')):
+        chunk_files.append(chunk_file)
+    # Sort files in numerical order
+    chunk_files.sort(key=lambda x: int(x.split("_")[-1]))
+
+    with open(file_path, 'wb') as destination:
+        for chunk in chunk_files:
+            with open(chunk, 'rb') as chunk_fd:
+                shutil.copyfileobj(chunk_fd, destination)
+            os.remove(chunk)
+
+    size = os.path.getsize(file_path)
+    file_metadata = dict(name=name, file=file_path, size=size)
+
+    # create a metadata_<uuid> file to store pickled metadata
+    metadata_file_path = os.path.join(upload_dir, 'metadata_' + file_uuid_name)
+    pickle.dump(file_metadata, open(metadata_file_path, 'wb'))
+    current_app.logger.warning("just uploaded: %s, size %d, in %s" % (name, size, file_path))
 
 def delete(request, sub_id):
     """
