943eacba56f707038d4567ef825712b4f095af02
Klara Peskova <ustav@seznam.cz>
2014-03-01 00:15:50 +0100

    Metadata form impoved (tooltip texts reviewed, placeholders added, delect field with dynamic text input field for 'Other...' option)

--- simplestore/lib/simplestore_marc_handler.py
+++ simplestore/lib/simplestore_marc_handler.py
@@ -42,54 +42,54 @@ def add_basic_fields(rec, form, email):
         if form['title']:
             record_add_field(rec, '245', subfields=[('a', remove_html_markup(form['title']))])
 
-        if form['creator']:
-            record_add_field(rec, '100', subfields=[('a', remove_html_markup(form['creator']))])
-
-        if form['domain']:
-            record_add_field(rec, '980', subfields=[('a', remove_html_markup(form['domain']))])
-        pubfields = []
-        if form['publisher']:
-            pubfields.append(('b', remove_html_markup(form['publisher'])))
-        if form.get('publication_date'):
-            pubfields.append(('c', remove_html_markup(form['publication_date'])))
-        if pubfields:
-            record_add_field(rec, '260', subfields=pubfields)
-        record_add_field(rec, '856', ind1='0', subfields=[('f', email)])
-
-        if 'open_access' in form:
-            record_add_field(rec, '542', subfields=[('l', 'open')])
-        else:
-            record_add_field(rec, '542', subfields=[('l', 'restricted')])
-
-        if form['licence']:
-            record_add_field(rec, '540', subfields=[('a', remove_html_markup(form['licence']))])
-        record_add_field(rec, '520', subfields=[('a', remove_html_markup(form['description']))])
-
-        if form['tags']:
-            for kw in form['tags'].split(','):
-                record_add_field(rec, '653',
-                                 ind1='1',
-                                 subfields=[('a', remove_html_markup(kw.strip()))])
-
-        if form['contributors']:
-            for kw in form['contributors'].split(';'):
-                record_add_field(rec, '700', subfields=[('a', remove_html_markup(kw.strip()))])
-
-        record_add_field(rec, '546', subfields=[('a', remove_html_markup(form['language']))])
-
-        # copying zenodo here, but I don't think 980 is the right MARC field
-        if form['resource_type']:
-            record_add_field(rec, '980', subfields=[('a', remove_html_markup(form['resource_type']))])
-
-        if form['alternate_identifier']:
-            record_add_field(rec, '024',
-                             subfields=[('a', remove_html_markup(form['alternate_identifier']))])
-
-        if form['version']:
-            record_add_field(rec, '250', subfields=[('a', remove_html_markup(form['version']))])
-        record_add_field(rec, '264',
-                         subfields=[('b', CFG_SITE_NAME),
-                                    ('c', str(datetime.utcnow()) + " UTC")])
+        # if form['creator']:
+        #     record_add_field(rec, '100', subfields=[('a', remove_html_markup(form['creator']))])
+        #
+        # if form['domain']:
+        #     record_add_field(rec, '980', subfields=[('a', remove_html_markup(form['domain']))])
+        # pubfields = []
+        # if form['publisher']:
+        #     pubfields.append(('b', remove_html_markup(form['publisher'])))
+        # if form.get('publication_date'):
+        #     pubfields.append(('c', remove_html_markup(form['publication_date'])))
+        # if pubfields:
+        #     record_add_field(rec, '260', subfields=pubfields)
+        # record_add_field(rec, '856', ind1='0', subfields=[('f', email)])
+        #
+        # if 'open_access' in form:
+        #     record_add_field(rec, '542', subfields=[('l', 'open')])
+        # else:
+        #     record_add_field(rec, '542', subfields=[('l', 'restricted')])
+        #
+        # if form['licence']:
+        #     record_add_field(rec, '540', subfields=[('a', remove_html_markup(form['licence']))])
+        # record_add_field(rec, '520', subfields=[('a', remove_html_markup(form['description']))])
+        #
+        # if form['tags']:
+        #     for kw in form['tags'].split(','):
+        #         record_add_field(rec, '653',
+        #                          ind1='1',
+        #                          subfields=[('a', remove_html_markup(kw.strip()))])
+        #
+        # if form['contributors']:
+        #     for kw in form['contributors'].split(';'):
+        #         record_add_field(rec, '700', subfields=[('a', remove_html_markup(kw.strip()))])
+        #
+        # record_add_field(rec, '546', subfields=[('a', remove_html_markup(form['language']))])
+        #
+        # # copying zenodo here, but I don't think 980 is the right MARC field
+        # if form['resource_type']:
+        #     record_add_field(rec, '980', subfields=[('a', remove_html_markup(form['resource_type']))])
+        # 
+        # if form['alternate_identifier']:
+        #     record_add_field(rec, '024',
+        #                      subfields=[('a', remove_html_markup(form['alternate_identifier']))])
+        #
+        # if form['version']:
+        #     record_add_field(rec, '250', subfields=[('a', remove_html_markup(form['version']))])
+        # record_add_field(rec, '264',
+        #                  subfields=[('b', CFG_SITE_NAME),
+        #                             ('c', str(datetime.utcnow()) + " UTC")])
     except Exception as e:
         current_app.logger.error(e)
         raise
