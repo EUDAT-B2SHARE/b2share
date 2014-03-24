40cc47fcb66cddf86fa3a96c83ede43527aa067e
Klara Peskova <ustav@seznam.cz>
2014-03-24 18:00:11 +0100

    Problem with % in file name fixed

--- invenio/lib/bibdocfile.py
+++ invenio/lib/bibdocfile.py
@@ -2901,12 +2901,13 @@ class BibDocFile(object):
             self.cd = self.md
 
         self.dir = os.path.dirname(fullpath)
+        url_name = urllib.quote(self.name)
         if self.subformat:
-            self.url = create_url('/%s/%s/files/%s%s' % (CFG_SITE_RECORD, self.recids_doctypes[0][0], self.name, self.superformat), {'subformat' : self.subformat})
-            self.fullurl = create_url('/%s/%s/files/%s%s' % (CFG_SITE_RECORD, self.recids_doctypes[0][0], self.name, self.superformat), {'subformat' : self.subformat, 'version' : self.version})
+            self.url = create_url('%s/%s/%s/files/%s%s' % (CFG_SITE_SECURE_URL, CFG_SITE_RECORD, self.recids_doctypes[0][0], url_name, self.superformat), {'subformat' : self.subformat})
+            self.fullurl = create_url('%s/%s/%s/files/%s%s' % (CFG_SITE_SECURE_URL, CFG_SITE_RECORD, self.recids_doctypes[0][0], url_name, self.superformat), {'subformat' : self.subformat, 'version' : self.version})
         else:
-            self.url = create_url('/%s/%s/files/%s%s' % (CFG_SITE_RECORD, self.recids_doctypes[0][0], self.name, self.superformat), {})
-            self.fullurl = create_url('/%s/%s/files/%s%s' % (CFG_SITE_RECORD, self.recids_doctypes[0][0], self.name, self.superformat), {'version' : self.version})
+            self.url = create_url('%s/%s/%s/files/%s%s' % (CFG_SITE_SECURE_URL, CFG_SITE_RECORD, self.recids_doctypes[0][0], url_name, self.superformat), {})
+            self.fullurl = create_url('%s/%s/%s/files/%s%s' % (CFG_SITE_SECURE_URL, CFG_SITE_RECORD, self.recids_doctypes[0][0], url_name, self.superformat), {'version' : self.version})
         self.etag = '"%i%s%i"' % (self.docid, self.format, self.version)
         self.magic = None
 
