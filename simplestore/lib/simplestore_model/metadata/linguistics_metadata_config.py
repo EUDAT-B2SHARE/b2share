a7bca71f26ea164ce3ee1972b488f91cda3b7453
root <root@192-168-17-4.(none)>
2014-03-24 18:40:25 +0200

    Add tooltips to the domains

--- simplestore/lib/simplestore_model/metadata/linguistics_metadata_config.py
+++ simplestore/lib/simplestore_model/metadata/linguistics_metadata_config.py
@@ -5,7 +5,6 @@ import json
 domain = "Linguistics"
 table_name = 'CLARIN'
 image = 'domain-clarin.png'
-description = 'This domain is for linguistic data.'
 
 fields = [{'name':'language_code',
            'display_text':'Language Code',
