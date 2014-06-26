e5f6fede96c8e3f7a6727d6b26e1f10e659cdbb8
Emanuel Dima <emanueldima@gmail.com>
2014-06-26 19:06:02 +0200

    added a bunch of config settings, including setting the collections in the database

--- invenio-pu/install_b2share.sh
+++ invenio-pu/install_b2share.sh
@@ -58,15 +58,52 @@ echo; echo "### Run inveniomanage config lessc and cleancss"
 inveniomanage config set LESS_BIN `find $PWD/node_modules -iname lessc | head -1`
 inveniomanage config set CLEANCSS_BIN `find $PWD/node_modules -iname cleancss | head -1`
 
-echo; echo "### Run inveniomanage database"
-inveniomanage database init --user=root --password=$MYSQL_ROOT --yes-i-know
-inveniomanage database create
-#inveniomanage demosite create
-
-echo; echo "### Config name and user"
+echo; echo "### Config site name"
 inveniomanage config set CFG_SITE_NAME B2SHARE
 inveniomanage config set CFG_SITE_NAME_INTL "{u'en' : u'B2SHARE'}"
+
+echo; echo "### Config bibsched user"
 inveniomanage config set CFG_BIBSCHED_PROCESS_USER $USER
+
+#echo; echo "### Config database"
+#inveniomanage config set CFG_DATABASE_HOST localhost
+#inveniomanage config set CFG_DATABASE_NAME invenio
+#inveniomanage config set CFG_DATABASE_USER root
+#inveniomanage config set CFG_DATABASE_PASS invenio
+
+# CFG_SITE_SECRET_KEY deprecated in favor of SECRET_KEY, created above ???
+#inveniomanage config set CFG_SITE_SECRET_KEY supersecretkeythatnoonecanguess
+
+echo; echo "### Config emails"
+inveniomanage config set CFG_SITE_ADMIN_EMAIL admin@localhost
+inveniomanage config set CFG_SITE_SUPPORT_EMAIL admin@localhost
+inveniomanage config set CFG_WEBALERT_ALERT_ENGINE_EMAIL admin@localhost
+inveniomanage config set CFG_WEBCOMMENT_ALERT_ENGINE_EMAIL admin@localhost
+
+echo; echo "### Config upload folder"
+inveniomanage config set CFG_SIMPLESTORE_UPLOAD_FOLDER /tmp/ss/
+
+echo; echo "### Config upload folder"
+inveniomanage config set CFG_SIMPLESTORE_DOMAINS [""]
+
+echo; echo "### Config epic credentials"
+inveniomanage config set CFG_EPIC_USERNAME ""
+inveniomanage config set CFG_EPIC_PASSWORD ""
+inveniomanage config set CFG_EPIC_BASEURL ""
+inveniomanage config set CFG_EPIC_PREFIX ""
+inveniomanage config set CFG_CAPTCHA_PRIVATE_KEY ""
+
+echo; echo "### Config captcha keys"
+inveniomanage config set CFG_CAPTCHA_PRIVATE_KEY ""
+inveniomanage config set CFG_CAPTCHA_PUBLIC_KEY ""
+
+echo; echo "### Run inveniomanage database"
+# needs to be run after the site name is set (root collection is CFG_SITE_NAME)
+inveniomanage database init --user=root --password=$MYSQL_ROOT --yes-i-know
+inveniomanage database create
+
+echo; echo "### Setup database collections"
+mysql -u root -D invenio --password=$MYSQL_ROOT < collections.sql
 
 echo; echo "### Config for development"
 inveniomanage config set CFG_EMAIL_BACKEND flask.ext.email.backends.console.Mail
