23318f6c35563a50325595512185002a572bd63b
Dennis Blommesteijn <dennis@blommesteijn.com>
2014-07-21 13:46:30 +0200

    Add variable invenio_dir

--- deployment/deploy_overlay.sh
+++ deployment/deploy_overlay.sh
@@ -13,25 +13,31 @@ WWW_USER=www-data
 #httpd on redhat and derivatives, apache2 on debian
 WWW_SERVICE=apache2
 
-cp -vr simplestore/lib/* /opt/invenio/lib/python/invenio/
-cp -vr simplestore/etc/static/* /opt/invenio/var/www/
-cp -vr simplestore/etc/templates/*.html /opt/invenio/etc/templates/
-cp -vr simplestore/etc/templates/*.markdown /opt/invenio/etc/templates/
+# set default path if none is set (in invenio-scripts)
+if [ "$INVENIO_DIR" = "" ] && [ "$1" = "" ]; then
+   INVENIO_DIR="/opt/invenio"
+elif [ "$1" != "" ]; then
+   INVENIO_DIR="$1"
+fi
+cp -vr simplestore/lib/* $INVENIO_DIR/lib/python/invenio/
+cp -vr simplestore/etc/static/* $INVENIO_DIR/var/www/
+cp -vr simplestore/etc/templates/*.html $INVENIO_DIR/etc/templates/
+cp -vr simplestore/etc/templates/*.markdown $INVENIO_DIR/etc/templates/
 
 # a quick hack for general modifications
 # XXX: invenio update unsafe
-if [ -f /opt/invenio/lib/python/invenio/bibfield_functions/is_type_isbn_issn_unit_tests.py ];
-  then rm -v /opt/invenio/lib/python/invenio/bibfield_functions/is_type_isbn_issn_unit_tests.py*;
+if [ -f $INVENIO_DIR/lib/python/invenio/bibfield_functions/is_type_isbn_issn_unit_tests.py ];
+  then rm -v $INVENIO_DIR/lib/python/invenio/bibfield_functions/is_type_isbn_issn_unit_tests.py*;
 fi
 
 # add invenio-specific overlay
-cp -vf invenio/templates/* /opt/invenio/etc/templates/
-cp -vrf invenio/lib/* /opt/invenio/lib/python/invenio/
-cp -vrf invenio/etc/* /opt/invenio/etc/
-cp -vrf invenio/var/* /opt/invenio/var/
+cp -vf invenio/templates/* $INVENIO_DIR/etc/templates/
+cp -vrf invenio/lib/* $INVENIO_DIR/lib/python/invenio/
+cp -vrf invenio/etc/* $INVENIO_DIR/etc/
+cp -vrf invenio/var/* $INVENIO_DIR/var/
 
-chown -R $WWW_USER.$WWW_USER /opt/invenio
+chown -R $WWW_USER.$WWW_USER $INVENIO_DIR
 
-pip install -r b2share_requirements.txt                                                               
+pip install -r b2share_requirements.txt
 
 service $WWW_SERVICE restart
