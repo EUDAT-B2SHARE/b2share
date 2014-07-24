2602e99d859375fcb01d10b4ed501473e7263bbd
Dennis Blommesteijn <dennis@blommesteijn.com>
2014-07-24 14:04:51 +0200

    Fix provisionfile

--- install/.vagrant/bootstrap/provision.sh
+++ install/.vagrant/bootstrap/provision.sh
@@ -1,7 +1,5 @@
-echo; echo "=+=+= Provisioning VM.... "
+echo; echo "=+=+= Provisioning VM =+=+="
 
 cd /home/vagrant/
 sudo /vagrant/provision_system.sh 2>&1 | tee provision.log
 sudo su vagrant -c "/vagrant/install_b2share.sh 2>&1 | tee install.log"
-sudo su vagrant -c "source /opt/python-2.7.6/bin/virtualenvwrapper.sh"
-sudo su vagrant -c "/vagrant/start_b2share.sh"
