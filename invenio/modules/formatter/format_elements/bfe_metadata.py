f6d937c9fb89683fa1a85c807104adc4cc1a88b2
Emanuel Dima <emanueldima@gmail.com>
2014-09-16 13:01:19 +0200

    fixed #384: reduced number of tabs, more information

---
+++ invenio/modules/formatter/format_elements/bfe_metadata.py
@@ -0,0 +1,94 @@
+## This file is part of SimpleStore.
+## Copyright (C) 2013 EPCC, The University of Edinburgh.
+##
+## SimpleStore is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## SimpleStore is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with SimpleStore; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""BibFormat element - Prints server info
+"""
+__revision__ = "$Id$"
+
+from invenio.b2share.modules.b2deposit.b2share_model import metadata_classes
+
+def format_element(bfo):
+
+    ret = '<div><table class="metadata_table table table-striped'\
+          ' table-condensed">'
+
+    ids = bfo.fields("0247_")
+    for i in ids:
+        val = i['a']
+        if i['2'] == "PID":
+            try:
+                val = '<a href="{0}">{0}</a>'.format(i['a'])
+            except ImportError:
+                None
+
+            ret += '<tr><th>{0}:</th><td>{1}</td></tr>'.format(
+                i['2'][0].upper() + i['2'][1:], val)
+
+    ver = bfo.field("250__a")
+    if ver:
+        ret += '<tr><th>Version:</th><td>{0}</td></tr>'.format(ver)
+
+    pub = bfo.field("260__")
+    if pub:
+        ret += '<tr><th>Publication:</th><td>{0}</td></tr>'.format(pub.get('b'))
+        if 'c' in pub:
+            ret += '<tr><th>Publication Date:</th><td>{0}</td></tr>'.format(
+                   pub['c'])
+
+    licence = bfo.field("540__a")
+    if licence:
+        ret += '<tr><th>Licence:</th><td>{0}</td></tr>'.format(licence)
+
+    uploader = bfo.field("8560_f")
+    if uploader:
+        ret += '<tr><th>Uploaded by:</th><td>{0}</td></tr>'.format(uploader)
+
+    contact_email = bfo.field("270__m")
+    if contact_email:
+        ret += '<tr><th>Contact email:</th><td>{0}</td></tr>'.format(contact_email)
+
+    domain = bfo.field("980__a")
+    if domain:
+        ret += '<tr><th>Domain:</th><td>{0}</td></tr>'.format(domain)
+
+    md_class = None
+    if domain.lower() in metadata_classes():
+        md_class = metadata_classes()[domain.lower()]()
+
+    domain_data = bfo.fields("690__")
+    for md in domain_data:
+        if md_class:
+            field = md_class.field_args.get(md['a'], {'label': md['a']})
+        else:
+            field = {'label': md['a']}
+        ret += '<tr><th>{0}:</th><td>{1}</td></tr>'.format(
+            field['label'], md['b'])
+
+    ids = bfo.fields("0247_")
+    for i in ids:
+        val = i['a']
+        if i['2'] != "PID":
+            ret += '<tr><th>{0}:</th><td>{1}</td></tr>'.format(
+                i['2'][0].upper() + i['2'][1:], val)
+
+    ret += '</table></div>'
+
+    return  ret
+
+
+def escape_values(bfo):
+    return 0
--- invenio/modules/formatter/templates/format/record/Default_HTML_detailed.tpl
+++ invenio/modules/formatter/templates/format/record/Default_HTML_detailed.tpl
@@ -51,7 +51,7 @@
     {{ bfe_appears_in_collections(bfo, prefix="<p style='margin-left: 10px;'><em>The record appears in these collections:</em><br />", suffix="</p>") }}
 
     {# WebTags #}
-    {{ tfn_webtag_record_tags(record['recid'], current_user.get_id())|prefix('<hr />') }}
+    {# {{ tfn_webtag_record_tags(record['recid'], current_user.get_id())|prefix('<hr />') }} #}
 
     {{ tfn_get_back_to_search_links(record['recid'])|wrap(prefix='<div class="pull-right linksbox">', suffix='</div>') }}
 {% endblock %}
