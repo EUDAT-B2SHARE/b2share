10dcf527906dbe3b3762b475f0b8ae2ba3b28437
Dennis Blommesteijn <dennis@blommesteijn.com>
2014-09-30 17:53:17 +0200

    Sync with invenio/next

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
  * This file is part of Invenio.
- * Copyright (C) 2011 CERN.
+ * Copyright (C) 2014 CERN.
  *
  * Invenio is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,8 +17,28 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  */
 
-CKEDITOR.plugins.setLang( 'scientificchar', 'en',
-{
-    title: 'Select Scientific Symbol',
-    toolbar: 'Insert Scientific Symbol'
-});
+'use strict';
+
+
+module.exports = function (grunt) {
+    var prefix = process.env.VIRTUAL_ENV || '../..'
+      , globalConfig = {
+            bower_path: 'bower_components',
+            installation_path: prefix + '/var/invenio.base-instance/static'
+        };
+
+    // target for custom path
+    if (grunt.option('path')) {
+        globalConfig.installation_path = grunt.option('target', grunt.option('path'))
+    }
+
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+
+    // load all grunt tasks
+    require('load-grunt-config')(grunt, {
+        config: {
+            globalConfig: globalConfig
+        }
+    });
+};
