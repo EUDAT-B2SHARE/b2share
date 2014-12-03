a9edea5442a7b71f0a4fb41af3724d4ff506d8e7
Dennis Blommesteijn <dennis@blommesteijn.com>
2014-12-03 17:57:18 +0100

    Move init user

--- invenio/b2share/testsuite/test_b2deposit_restapi.py
+++ invenio/b2share/testsuite/test_b2deposit_restapi.py
@@ -20,20 +20,24 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 
 
 class RestApi(object):
+    """rest api wrapper class"""
 
     def __init__(self, url=None, access_token=None):
         self.url = url + "/api"
         self.at = access_token
-        # self.prefix = "/api"
 
     def get_deposits(self):
-        r = requests.get(self.url + "/depositions/?access_token=" + self.at)
-        return r
+        try:
+            r = requests.get(self.url + "/depositions/?access_token=" + self.at)
+            return r
+        except:
+            print "server not found at: `" + self.url + "`"
+        return None
 
-class TestB2depositRestapiConnect(InvenioTestCase):
-    """Unit tests for restapi connecting"""
+class InitHelper(object):
 
-    def setUp(self):
+    @staticmethod
+    def init_user_token(test_case):
         # get user
         admin_user = User.query.filter_by(email='admin@localhost').first()
         if admin_user == None:
@@ -41,7 +45,7 @@ class TestB2depositRestapiConnect(InvenioTestCase):
             admin_user = User(email="admin@localhost", note="1", nickname="admin", password="admin")
             db.session.add(admin_user)
             db.session.commit()
-        self.assertIsNotNone(admin_user)
+        test_case.assertIsNotNone(admin_user)
         admin_user_id = admin_user.get_id()
         # get token
         token = Token.query.filter_by(user_id=admin_user_id).first()
@@ -49,7 +53,7 @@ class TestB2depositRestapiConnect(InvenioTestCase):
             # create client object
             client = Client(name="test_client", user_id=admin_user_id, is_internal=True,
                 is_confidential=False)
-            self.assertIsNotNone(client)
+            test_case.assertIsNotNone(client)
             client.gen_salt()
             db.session.add(client)
             # create login token
@@ -60,10 +64,17 @@ class TestB2depositRestapiConnect(InvenioTestCase):
             db.session.add(token)
             db.session.commit()
         # verify info
-        self.assertIsNotNone(token)
-        self.assertTrue(len(token.access_token) == 60)
-        self.access_token = token.access_token
-        self.current_app_url = current_app.config.get('CFG_SITE_URL')
+        test_case.assertIsNotNone(token)
+        test_case.assertTrue(len(token.access_token) == 60)
+        test_case.access_token = token.access_token
+        test_case.current_app_url = current_app.config.get('CFG_SITE_URL')
+
+
+class TestB2depositRestapiConnect(InvenioTestCase):
+    """Unit tests for restapi connecting"""
+
+    def setUp(self):
+        InitHelper.init_user_token(self)
 
     def test_connect_with_oauth(self):
         api = RestApi(url=self.current_app_url, access_token=self.access_token)
@@ -72,11 +83,6 @@ class TestB2depositRestapiConnect(InvenioTestCase):
         self.assertEqual(r.status_code, 200)
         body_json = r.json()
         self.assertTrue(isinstance(body_json,list))
-        # print r
-
-
-
-        # print(self.access_token)
 
     # TODO: add get token handle
     # def test_get_token(self):
@@ -85,6 +91,9 @@ class TestB2depositRestapiConnect(InvenioTestCase):
 
 class TestB2depositRestapiRecord(InvenioTestCase):
     """Unit tests for restapi record"""
+
+    def setUp(self):
+        InitHelper.init_user_token(self)
 
     def test_records(self):
         # TODO: implement!
@@ -116,7 +125,7 @@ class TestB2depositRestapiRecord(InvenioTestCase):
 
 TEST_SUITE = make_test_suite(
                 TestB2depositRestapiConnect,
-                # TestB2depositRestapiRecord
+                TestB2depositRestapiRecord
             )
 
 if __name__ == "__main__":
