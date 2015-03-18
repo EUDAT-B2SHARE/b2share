0d633bd7472d9f364cc9b09673eaa3292e1a7c14
Mikael Karlsson <i8myshoes@gmail.com>
2015-03-18 17:01:13 +0200

    install: Script '980to337' create multiple fields

--- install/980to337.py
+++ install/980to337.py
@@ -2,9 +2,12 @@
 Loop over all records in Invenio and move 'resource type' metadata from '980' to '337'
 """
 
+from pprint import pprint
 import itertools
 
 from invenio.base.factory import with_app_context
+
+TYPES = ['Text', 'Image', 'Video', 'Audio', 'Time-Series', 'Other']
 
 
 @with_app_context()
@@ -34,20 +37,28 @@ def main():
         new_337 = []
         for f in rec.get('980', []):
             for sf in f[0]:
-                if sf[0] == 'a' and sf[1] in ['Text', 'Image', 'Video', 'Audio', 'Time-Series', 'Other']:
-                    new_337.append(sf)
+                if sf[0] == 'a' and sf[1] in TYPES:
+                    if [sf] not in new_337:
+                        new_337.append([sf])
                 else:
-                    new_980.append(sf)
+                    if [sf] not in new_980:
+                        new_980.append([sf])
 
         if new_337:
             record_delete_field(rec, '980')
-            record_add_field(rec, '337', subfields=new_337)
-            record_add_field(rec, '980', subfields=new_980)
+            for f in new_337:
+                record_add_field(rec, '337', subfields=f)
+            for f in new_980:
+                record_add_field(rec, '980', subfields=f)
+            print('\nOld 337:')
+            pprint(old_rec.get('337'))
+            print('New 337:')
+            pprint(rec.get('337'))
 
-            print('Old 337: {0}'.format(old_rec.get('337')))
-            print('New 337: {0}'.format(rec.get('337')))
-            print('Old 980: {0}'.format(old_rec.get('980')))
-            print('New 980: {0}'.format(rec.get('980')))
+            print('\nOld 980:')
+            pprint(old_rec.get('980'))
+            print('New 980:')
+            pprint(rec.get('980'))
             if raw_input('Bibupload (y/n)? ') == 'y':
                 bibupload(rec, 'replace')
 
