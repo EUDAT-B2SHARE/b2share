f0c311789b3ae6472146f42892ab4a0bba59137a
Emanuel Dima <emanueldima@gmail.com>
2015-04-06 20:19:04 +0200

    bug fixes

--- install-invenio2/sync.sh
+++ install-invenio2/sync.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+HOST=default
+
 SOURCE=../../b2share/
 TARGET=/home/vagrant/.virtualenvs/b2share/src/
-HOST=default
 echo "Copying $SOURCE to $HOST:$TARGET"
 scp -q -F .sshconfig -r $SOURCE vagrant@$HOST:$TARGET
+
+SOURCE=../install-invenio2/start_b2share.sh
+TARGET=/vagrant
+echo "Copying $SOURCE to $HOST:$TARGET"
+scp -q -F .sshconfig $SOURCE vagrant@$HOST:$TARGET
