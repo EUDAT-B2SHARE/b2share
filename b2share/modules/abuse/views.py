e9ddd7b249a61f0ab574b8a20148425484cacec9
Nicolas Harraudeau <nicolas.harraudeau@cern.ch>
2015-08-04 17:06:40 +0200

    global: split Invenio and B2Share
    * Moves B2Share files outside of Invenio directory.
    
    * Removes Invenio files.
    
    * Fixes B2Share configuration so that it can be used as an overlay.
    
    * Note: invenio modifications will be reproduced on B2Share's
      Invenio fork.
    
    Signed-off-by: Nicolas Harraudeau <nicolas.harraudeau@cern.ch>

---
+++ b2share/modules/abuse/views.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+## This file is part of B2SHARE.
+## Copyright (C) 2013 EPCC, The University of Edinburgh.
+##
+## B2SHARE is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## B2SHARE is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with B2SHARE; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+""" B2SHARE Abuse Form Blueprint"""
+from flask import request, Blueprint
+from flask.ext.breadcrumbs import register_breadcrumb
+from invenio.base.i18n import _
+import b2share.modules.abuse.abuse_form as abuse
+
+blueprint = Blueprint('abuse_form', __name__, 
+                      url_prefix='/abuse',
+                      template_folder='templates')
+
+@blueprint.route('/', methods=['GET'])
+def abuse_form_noparams():
+	return abuse.abuse_form(request,-1)
+
+@blueprint.route('/<recid>',methods=['GET'])
+@register_breadcrumb(blueprint, 'breadcrumbs.abuse', _('Abuse'))
+def abuse_form(recid):
+	return abuse.abuse_form(request,recid)
+
+@blueprint.route('/submit',methods=['POST'])
+def abuse_submit():
+	return abuse.abuse_submit(request)
