ddf6054c9022aab4d07381603c9908f12738581d
Nicolas Harraudeau <nicolas.harraudeau@cern.ch>
2015-08-13 14:15:19 +0200

    global: retrieve invenio config modifications
    * These modifications had been done on Invenio itself before the
      split.
    
    Signed-off-by: Nicolas Harraudeau <nicolas.harraudeau@cern.ch>

--- b2share/config.py
+++ b2share/config.py
@@ -7,6 +7,29 @@ PACKAGES = [
 ] + _PACKAGES
 
 
+PACKAGES_EXCLUDE = [
+    'invenio.modules.annotations',
+    'invenio.modules.archiver',
+    'invenio.modules.communities',
+    'invenio.modules.linkbacks',
+    'invenio.modules.multimedia',
+    'invenio.modules.pages',
+    'invenio.modules.deposit',
+]
+
+from b2share.modules.oauthclient import unity
+
+OAUTHCLIENT_REMOTE_APPS = dict(
+    unity=unity.REMOTE_APP,
+)
+
+UNITY_APP_CREDENTIALS = dict(
+    # will only work on development configurations
+    consumer_key= "b2share",
+    consumer_secret= "b2share8juelich",
+)
+
 # DEPOSIT_TYPES = [
 #     'invenio_demosite.modules.deposit.workflows.article.article',
 # ]
+
