8357762d1e7b4d016c30248dcec0f94faa92b329
Nicolas Harraudeau <nicolas.harraudeau@cern.ch>
2016-01-19 17:43:58 +0100

    global: application creation fix
    * FIX Fixes application creation in order to be compatible with new
      invenio modules.
    
    Signed-off-by: Nicolas Harraudeau <nicolas.harraudeau@cern.ch>

--- setup.py
+++ setup.py
@@ -1,40 +1,21 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of Invenio.
-# Copyright (C) 2013, 2014, 2015 CERN.
+# This file is part of EUDAT B2Share.
+# Copyright (C) 2015, 2016 CERN.
 #
-# Invenio is free software; you can redistribute it and/or
+# B2Share is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of the
 # License, or (at your option) any later version.
 #
-# Invenio is distributed in the hope that it will be useful, but
+# B2Share is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Invenio; if not, write to the Free Software Foundation, Inc.,
+# along with B2Share; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-
-# """Invenio is a framework for digital libraries and data repositories.
-
-# Invenio enables you to run your own digital library or document
-# repository on the web.  Invenio covers all aspects of digital library
-# management, from document ingestion, through classification, indexing
-# and further processing, to curation, archiving, and dissemination.
-# The flexibility and performance of Invenio make it a comprehensive
-# solution for management of document repositories of moderate to large
-# sizes (several millions of records).
-
-# Links
-# -----
-
-# * `website <http://invenio-software.org/>`_
-# * `documentation <http://invenio.readthedocs.org/en/latest/>`_
-# * `development <https://github.com/inveniosoftware/invenio>`_
-
-# """
 
 """B2Share application
 """  # FIXME improve this documentation
@@ -42,32 +23,90 @@
 import os
 import sys
 
-from distutils.command.build import build
-
 from setuptools import find_packages, setup
-from setuptools.command.install_lib import install_lib
+from setuptools.command.test import test as TestCommand
+
+install_requires = [
+    'invenio-config>=1.0.0a1,<1.1.0',
+    'invenio-base>=1.0.0a5,<1.1.0',
+    'invenio-records-rest>=1.0.0a3,<1.1.0',
+    'invenio-records>=1.0.0a8,<1.1.0',
+    'invenio-db>=1.0.0a9,<1.1.0',
+    'invenio-celery>=1.0.0a3,<1.1.0',
+    'httplib2',
+    'simplejson',
+]
+
+tests_require = [
+    'check-manifest>=0.25',
+    'coverage>=4.0',
+    'isort>=4.2.2',
+    'pep257>=0.7.0',
+    'pytest-cache>=1.0',
+    'pytest-cov>=1.8.0',
+    'pytest-pep8>=1.0.6',
+    'pytest>=2.8.0',
+    'nose',
+    'Flask-Testing'
+]
+
+extras_require = {
+    'mysql': [
+        'pymysql>=0.6.7',
+    ],
+    'postgresql': [
+        'psycopg2>=2.6.1',
+    ],
+    'docs': [
+        "Sphinx>=1.3",
+    ],
+    'development': [
+        'Flask-DebugToolbar>=0.9',
+        'setuptools-bower>=0.2'
+    ],
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+for reqs in extras_require.values():
+    extras_require['all'].extend(reqs)
+
+setup_requires = [
+]
 
 
-# class _build(build):  # noqa
+class PyTest(TestCommand):
+    """PyTest Test."""
 
-#     """Compile catalog before building the package."""
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
-#     sub_commands = [('compile_catalog', None)] + build.sub_commands
+    def initialize_options(self):
+        """Init pytest."""
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+        try:
+            from ConfigParser import ConfigParser
+        except ImportError:
+            from configparser import ConfigParser
+        config = ConfigParser()
+        config.read('pytest.ini')
+        self.pytest_args = config.get('pytest', 'addopts').split(' ')
 
+    def finalize_options(self):
+        """Finalize pytest."""
+        TestCommand.finalize_options(self)
+        if hasattr(self, '_test_args'):
+            self.test_suite = ''
+        else:
+            self.test_args = []
+            self.test_suite = True
 
-# class _install_lib(install_lib):  # noqa
-
-#     """Custom install_lib command."""
-
-#     def run(self):
-#         """Compile catalog before running installation command."""
-#         install_lib.run(self)
-#         self.run_command('compile_catalog')
-
-
-# install_requires = [
-
-# ]
+    def run_tests(self):
+        """Run tests."""
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 # extras_require = {
@@ -79,82 +118,35 @@ with open(os.path.join("b2share", "version.py"), "rt") as fp:
     exec(fp.read(), g)
 version = g["__version__"]
 
-# packages = find_packages(exclude=['docs'])
-# packages.append('invenio_docs')
-
 setup(
     name='b2share',
     version=version,
     url='https://github.com/EUDAT-B2SHARE/b2share',
     license='GPLv2',
     author='CERN',
-    # author_email='info@invenio-software.org',
     description='B2Share application',
     long_description=__doc__,
-    # packages=packages,
-    # package_dir={'invenio_docs': 'docs'},
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     platforms='any',
     entry_points={
+        'console_scripts': [
+            'b2share = b2share.cli:cli',
+        ],
         'invenio_base.api_apps': [
             'b2share_communities = b2share.modules.communities:B2ShareCommunities',
             'b2share_schemas = b2share.modules.schemas:B2ShareSchemas',
             'b2share_users = b2share.modules.users:B2ShareUsers',
-            'b2share_records = b2share.modules.records:B2ShareRecords',
         ],
+        # 'invenio_db.models': [
+        #     'b2share_communities = b2share.modules.communities.models',
+        # ],
     },
-    install_requires=[
-        # 'invenio>2.0',
-        # 'invenio-oaiharvester',
-        # 'invenio-oauth2server',
-        'invenio-config>=1.0.0a1,<1.1.0',
-        'invenio-base>=1.0.0a2,<1.1.0',
-        'invenio-records-rest>=1.0.0a2,<1.1.0',
-        'invenio-records>=1.0.0a2,<1.1.0',
-        'invenio-db>=1.0.0a5,<1.1.0',
-        'invenio-celery>=1.0.0a1,<1.1.0',
-        # 'markdown2',
-        # 'validate_email',
-        # 'recaptcha-client',
-        # 'markdown',
-        'httplib2',
-        'simplejson',
-        'jsonschema',
-    ],
-    extras_require={
-        'development': [
-            'Flask-DebugToolbar>=0.9',
-            'setuptools-bower>=0.2'
-        ],
-    },
-    test_suite='nose.collector',
-    tests_require=[
-        'nose',
-        'Flask-Testing'
-    ],
+    extras_require=extras_require,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
 
-
-    # setup_requires=setup_requires,
-    # install_requires=install_requires,
-    # extras_require=extras_require,
-    # classifiers=[
-    #     'Development Status :: 4 - Beta',
-    #     'Environment :: Web Environment',
-    #     'Intended Audience :: Developers',
-    #     'License :: OSI Approved :: GNU General Public License v2'
-    #     ' or later (GPLv2+)',
-    #     'Operating System :: OS Independent',
-    #     'Programming Language :: Python',
-    #     'Programming Language :: Python :: 2',
-    #     'Programming Language :: Python :: 2.6',
-    #     'Programming Language :: Python :: 2.7',
-    #     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    # ],
-    # test_suite='invenio.testsuite.suite',
-    # tests_require=tests_require,
-    # cmdclass={
-    #     'build': _build,
-    #     'install_lib': _install_lib,
-    # },
+    cmdclass={'test': PyTest},
 )
