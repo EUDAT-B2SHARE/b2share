165a693677d084ce811fb0e119b367002f6a90f7
Nicolas Harraudeau <nicolas.harraudeau@cern.ch>
2016-02-16 15:06:36 +0100

    global: CERN file headers fix
    * Adds CERN specific clauses in file headers.
    
    Signed-off-by: Nicolas Harraudeau <nicolas.harraudeau@cern.ch>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of EUDAT B2Share.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2015, 2016, University of Tuebingen, CERN.
 #
 # B2Share is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with B2Share; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 """B2Share application
 """  # FIXME improve this documentation
