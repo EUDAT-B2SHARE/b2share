c4eaa86390a13010681be68c53162efd1ae50675
Emanuel Dima <emanueldima@gmail.com>
2016-04-21 15:30:28 +0200

    new webui lib structure for vendor assets

--- webui/copy_files.sh
+++ webui/copy_files.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 echo Copying files...
 
-CSS_DIR=app/lib/
-FONTS_DIR=app/fonts/
+CSS_DIR=app/lib/css
+FONTS_DIR=app/lib/fonts
+IMG_DIR=app/lib/img
 
-mkdir -p $CSS_DIR $FONTS_DIR
+mkdir -p $CSS_DIR $FONTS_DIR $IMG_DIR
 
-cp node_modules/bootstrap/dist/css/* $CSS_DIR
-cp node_modules/bootstrap/dist/fonts/* $FONTS_DIR
+cp node_modules/bootstrap/dist/css/*   $CSS_DIR/
+cp node_modules/bootstrap/dist/fonts/* $FONTS_DIR/
 
-cp node_modules/font-awesome/css/* $CSS_DIR
-cp node_modules/font-awesome/fonts/*  $FONTS_DIR
+cp node_modules/font-awesome/css/*   $CSS_DIR/
+cp node_modules/font-awesome/fonts/* $FONTS_DIR/
 
 cp node_modules/react-toggle/style.css $CSS_DIR/toggle-style.css
+
+cp -r node_modules/react-widgets/dist/css/*   $CSS_DIR/
+cp -r node_modules/react-widgets/dist/fonts/* $FONTS_DIR/
+cp -r node_modules/react-widgets/dist/img/*   $IMG_DIR/
 
 # cp node_modules/pluralize/pluralize.js $JS_DIR
 # cp node_modules/react/dist/react-with-addons.* $JS_DIR
