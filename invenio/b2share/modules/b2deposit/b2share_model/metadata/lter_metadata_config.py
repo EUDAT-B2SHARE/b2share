bf704eed91dd10e4f6d8a33f548aff52ccf4f160
llehtine <lassi.lehtinen@csc.fi>
2016-05-10 16:16:30 +0300

    Update lter_metadata_config.py

--- invenio/b2share/modules/b2deposit/b2share_model/metadata/lter_metadata_config.py
+++ invenio/b2share/modules/b2deposit/b2share_model/metadata/lter_metadata_config.py
@@ -1,6 +1,18 @@
+from invenio.ext.sqlalchemy import db
+
 domain = "LTER"
 display_name = "LTER"
 table_name = 'lter'
 image = 'domain-lter.jpg'
 kind = 'project'
 domaindesc = 'LTER Europe'
+
+fields = [
+    {
+        'name': 'metadata_url',
+        'col_type': db.String(256),
+        'display_text': 'Metadata URL',
+        'description': 'Metadata URL',
+        'required': True,
+    },
+]
