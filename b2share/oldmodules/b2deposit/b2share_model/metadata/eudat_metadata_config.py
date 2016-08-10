e4d07f324d92a487528f61ec7dffd91e79c3925b
Emanuel Dima <emanueldima@gmail.com>
2016-08-10 12:10:21 +0200

    remove oldmodules, legacy b2share v1 code

--- b2share/oldmodules/b2deposit/b2share_model/metadata/eudat_metadata_config.py
+++
@@ -1,6 +0,0 @@
-domain = "generic"
-display_name = "Generic"
-table_name = 'eudat'
-image = 'domain-eudat.png'
-kind = 'project'
-domaindesc = 'A generic domain to be used when no specific domain is appropriate'
