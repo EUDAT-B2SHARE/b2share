c9c6c57bca5b72a68bdf8e92a8cc6c61d68ab18c
Thijs Cobben <thijscobben@gmail.com>
2016-09-07 13:05:58 +0200

    Removed my API token for v1...

--- demo/re-import.sh
+++ demo/re-import.sh
@@ -7,4 +7,4 @@ b2share index init
 b2share schemas init
 b2share demo load_config -f
 b2share demo load_data
-b2share demo import_v1_data -v -d IoiST2yAgsmLKjygtUm2NFGaMKK82IcnLidZWnOsHV1PcLJzeSqQOqL6B8hr /tmp/b2sharev1
+b2share demo import_v1_data -v -d <token> /tmp/b2sharev1
