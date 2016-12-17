c4221b2c0f23b9113bacd502e2a9e0daecb721e7
Nicolas Harraudeau <nicolas.harraudeau@cern.ch>
2016-12-17 12:49:54 +0100

    global: root schema constraints fix
    * FIX Makes community_specific field required (closes #1067).
    
    * FIX Forbids additional properties in every object field of the
      root schema. (closes #1238)
    
    Signed-off-by: Nicolas Harraudeau <nicolas.harraudeau@cern.ch>

--- tests/b2share_unit_tests/records/test_records_api.py
+++ tests/b2share_unit_tests/records/test_records_api.py
@@ -30,6 +30,7 @@ import pytest
 from jsonschema.exceptions import ValidationError
 from b2share.modules.records.errors import AlteredRecordError
 from invenio_records import Record
+from b2share.modules.deposit.api import Deposit
 
 
 def test_change_record_schema_fails(app, test_records):
@@ -54,3 +55,43 @@ def test_change_record_community(app, test_records):
         record['community'] = str(uuid.uuid4())
         with pytest.raises(AlteredRecordError):
             record.commit()
+
+
+def test_record_add_unknown_fields(app, test_records):
+    """Test adding unknown fields in a record. It should fail."""
+    for path in [
+        # all the following paths point to "object" fields in
+        # in the root JSON Schema
+        '/new_field',
+        '/community_specific/new_field',
+        '/creators/0/new_field',
+        '/titles/0/new_field',
+        '/contributors/0/new_field',
+        '/resource_types/0/new_field',
+        '/alternate_identifiers/0/new_field',
+        '/descriptions/0/new_field',
+        '/license/new_field',
+    ]:
+        with app.app_context():
+            record = Record.get_record(test_records[0].record_id)
+            record = record.patch([
+                {'op': 'add', 'path': path, 'value': 'any value'}
+            ])
+            with pytest.raises(ValidationError):
+                record.commit()
+
+
+def test_record_commit_with_incomplete_metadata(app,
+                                                test_incomplete_records_data):
+    """Test commit of an incomplete record fails."""
+    for data in test_incomplete_records_data:
+        with app.app_context():
+            deposit = Deposit.create(data.complete_data)
+            deposit.submit()
+            deposit.publish()
+            deposit.commit()
+            pid, record = deposit.fetch_published()
+            # make the data incomplete
+            record = record.patch(data.patch)
+            with pytest.raises(ValidationError):
+                record.commit()
