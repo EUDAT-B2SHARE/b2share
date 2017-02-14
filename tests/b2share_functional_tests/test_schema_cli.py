2dcbe88ec5a02ea40c220a4cdc38226b859ef21b
Thijs Cobben <thijscobben@gmail.com>
2017-02-14 11:41:32 +0100

    Towards unit test

---
+++ tests/b2share_functional_tests/test_schema_cli.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of EUDAT B2Share.
+# Copyright (C) 2016 CERN.
+#
+# B2Share is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# B2Share is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with B2Share; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Test B2Share demonstration module."""
+
+import pytest
+from click.testing import CliRunner
+from flask_cli import ScriptInfo
+
+from b2share.modules.schemas.cli import schemas as schemas_cmd
+
+
+@pytest.mark.parametrize('app', [({
+    'config': {'PREFERRED_URL_SCHEME': 'https'}
+}), ({
+    'config': {'PREFERRED_URL_SCHEME': 'http'}
+})],
+    indirect=['app'])
+
+def test_set_schema_cmd(app, test_communities):
+    """Test the `schemas set_schema` CLI command."""
+    with app.app_context():
+        runner = CliRunner()
+        script_info = ScriptInfo(create_app=lambda info: app)
+
+        # Run 'set schema' command
+        with runner.isolated_filesystem():
+            result = runner.invoke(schemas_cmd, ['set_schema','Aalto','clarin.json'], obj=script_info)
+            click.secho(result)
+            assert result.exit_code == 0
+    
+    
