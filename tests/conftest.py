8e617ac34908a12618e8ac35f40aeead85ba6a2b
Thijs Cobben <thijscobben@gmail.com>
2017-10-23 11:02:41 +0200

    Removed my own test settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -86,9 +86,7 @@ def base_app():
         DEBUG_TB_ENABLED=False,
         SQLALCHEMY_DATABASE_URI=os.environ.get(
             'SQLALCHEMY_DATABASE_URI',
-            #'postgresql+psycopg2://postgres@localhost:5432/b2sharedb'),
-            'sqlite://'),
-        FLASK_DEBUG=0,
+            'postgresql+psycopg2://postgres@localhost:5432/b2sharedb'),
         LOGIN_DISABLED=False,
         WTF_CSRF_ENABLED=False,
         SECRET_KEY="CHANGE_ME",
