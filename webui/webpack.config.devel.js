dd3b3f0493b6159202a51634251412185d51e0e2
hansp <hans.vanpiggelen@surfsara.nl>
2019-01-16 15:23:43 +0100

    Update webpack configuration

--- webui/webpack.config.devel.js
+++ webui/webpack.config.devel.js
@@ -4,6 +4,10 @@ var webpack = require('webpack');
 module.exports = {
     entry: ['./src/main.jsx'],
     devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        open: true, // to open the local server in browser
+        contentBase: __dirname + '/app',
+      },
     output: { path: __dirname+"/app", filename: 'b2share-bundle.js' },
     plugins: [
     ],
