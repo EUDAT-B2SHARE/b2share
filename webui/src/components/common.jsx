7818606b2d5cba771a0a82cf8f1662d153f33d4f
hevp <hevp101@outlook.com>
2020-02-14 13:27:46 +0100

    Allow HTML as ImplodedList delimiter

--- webui/src/components/common.jsx
+++ webui/src/components/common.jsx
@@ -73,7 +73,7 @@ export const ImplodedList = React.createClass({
             <span>
                 { this.props.data
                       .map((t, index) => <span key={index}>{t}</span>)
-                      .reduce((prev, curr) => [prev, this.props.delim, curr])
+                      .reduce((prev, curr) => [prev, <span dangerouslySetInnerHTML={{__html: this.props.delim}}/>, curr])
                 }
             </span>
         )
