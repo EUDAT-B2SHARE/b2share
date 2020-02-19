08d2598b96a845802a2ef2e3e761ecf7dc699e60
hevp <hevp101@outlook.com>
2020-02-19 17:42:58 +0100

    Generalise help menu links generation

--- webui/src/components/help.jsx
+++ webui/src/components/help.jsx
@@ -1,40 +1,38 @@
 import React from 'react/lib/ReactWithAddons';
 import { Link } from 'react-router';
-
 import { serverCache, Error } from '../data/server';
 
 
 export const Help = React.createClass({
+    listLinks(links) {
+        return <ul className="list-group">
+            { Object.keys(links).map((key, index) => (
+                !links[key][0] ? false :
+                <li className="list-group-item" key={index}>
+                    <Link to={links[key][0]} target="_blank" key={index}><i className={"fa " + links[key][1]}></i> {key}</Link>
+                </li>
+            ))}
+            </ul>
+    },
+
     render() {
-        const termsOfUse = serverCache.getInfo().get('terms_of_use_link');
+        const info = serverCache.getInfo();
+        const helplinks = info.get('help_links', new Map);
+        const links = {
+            'What is B2SHARE?': ['https://eudat.eu/services/b2share', 'fa-list'],
+            'User Guide': [helplinks.get('user-guide'), 'fa-file'],
+            'Search': [helplinks.get('search'), 'fa-info-circle'],
+            'REST API': [helplinks.get('rest-api'), 'fa-info-circle'],
+            'Terms of Use': [info.get('terms_of_use_link'), 'fa-question-circle'],
+            'Report an issue': [helplinks.get('issues'), 'fa-exclamation-circle']
+        }
+
         return (
             <div className="help-page">
                 <h1>Help</h1>
-
                 <div className="container-fluid">
                     <div className="col-sm-6">
-                        <ul className="list-group">
-                            <li className="list-group-item">
-                                <Link to="https://eudat.eu/services/b2share" target="_blank"><i className="fa fa-list"></i> What is B2SHARE?</Link>
-                            </li>
-                            <li className="list-group-item">
-                                <Link to="https://eudat.eu/services/userdoc/b2share-usage" target="_blank"><i className="fa fa-info-circle"></i> User Guide</Link>
-                            </li>
-                            <li className="list-group-item">
-                                <Link to="https://eudat.eu/services/userdoc/b2share-advanced-search" target="_blank"><i className="fa fa-info-circle"></i> Advanced Search guide </Link>
-                            </li>
-                            <li className="list-group-item">
-                                <Link to="https://eudat.eu/services/userdoc/b2share-http-rest-api" target="_blank"><i className="fa fa-list"></i> REST API </Link>
-                            </li>
-                            { termsOfUse ?
-                                <li className="list-group-item">
-                                    <a href={termsOfUse}><i className="fa fa-question-circle"></i> Terms of Use</a>
-                                </li> : false
-                            }
-                            <li className="list-group-item">
-                                <Link to="https://github.com/EUDAT-B2SHARE/b2share" target="_blank"><i className="fa fa-exclamation-circle"></i> Report an issue </Link>
-                            </li>
-                        </ul>
+                        {this.listLinks(links)}
                     </div>
                 </div>
             </div>
