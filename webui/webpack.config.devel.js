f62bf56e563127f6139bde1075c499c08b7d15f3
Jussi Steenari <jsteenar@csc.fi>
2021-02-22 17:10:30 +0200

    webui: Add cache invalidation support
    * use webpack hash functionality to generate unique filenames
      for `b2share-bundle.js`
    * index.html created from template (html-webpack-plugin)
    
    Signed-off-by: Jussi Steenari <jsteenar@csc.fi>
    Co-authored-by: Harri Hirvonsalo <harri.hirvonsalo@csc.fi>

--- webui/webpack.config.devel.js
+++ webui/webpack.config.devel.js
@@ -2,13 +2,22 @@ var path = require('path');
 var webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 var WatchTimePlugin = require('webpack-watch-time-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 console.log("Using configuration file from", __dirname+'/../webui.cfg');
+
+let cleanOptions = {
+  root: __dirname+'/app',
+  verbose: true,
+  dry: false
+}
+
 
 module.exports = {
     entry: ['./src/main.jsx'],
     devtool: 'cheap-module-eval-source-map',
-    output: { path: __dirname+"/app", filename: 'b2share-bundle.js' },
+    output: { path: __dirname+"/app/", filename: 'js/b2share-bundle.[hash].js'},
     plugins: [
         // Note: Only values used in source code will be included in the bundle.
         // See dotenv-webpack documentation for more details.
@@ -16,7 +25,16 @@ module.exports = {
             path: __dirname+'/../webui.cfg', // load this file instead of '.env'.
             systemvars: true, // load environment variables from 'process.env'.
         }),
-        WatchTimePlugin
+        WatchTimePlugin,
+        new CleanWebpackPlugin('js', cleanOptions),
+        //inject false prevents script tag from being automatically injected
+        //the script tag is injected directly in index_template.html code instead
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: false,
+            template: 'templates/index_template.html',
+            filename: 'index.html'
+        }),
     ],
     module: {
         loaders: [
--- webui/webpack.config.js
+++ webui/webpack.config.js
@@ -1,13 +1,23 @@
 var path = require('path');
 var webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 console.log("Using configuration file from", __dirname+'/../webui.cfg');
+
+let cleanOptions = {
+  root: __dirname+'/app',
+  verbose: true,
+  dry: false
+}
+
+
 
 module.exports = {
     entry: ['./src/main.jsx'],
     devtool: 'source-map',
-    output: { path: __dirname+"/app", filename: 'b2share-bundle.js' },
+    output: { path: __dirname+"/app/", filename: 'js/b2share-bundle.[hash].js'},
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
@@ -22,6 +32,17 @@ module.exports = {
         }),
         new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/), // trim down moment.js
         new webpack.optimize.OccurenceOrderPlugin(),
+         
+        new CleanWebpackPlugin('js', cleanOptions),
+
+        //inject false prevents script tag from being automatically injected
+        //the script tag is injected directly in index_template.html code instead
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: false,
+            template: 'templates/index_template.html',
+            filename: 'index.html'
+        }),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
