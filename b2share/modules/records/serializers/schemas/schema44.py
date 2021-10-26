2a412846796a2ee681359a5d4e488b240578892a
Janne Karjalainen <janne.karjalainen@csc.fi>
2021-10-26 14:39:53 +0300

    Fix DataCite 4.4 serialization

--- b2share/modules/records/serializers/schemas/schema44.py
+++ b2share/modules/records/serializers/schemas/schema44.py
@@ -1,6 +1,44 @@
 from datacite.schema43 import *
+import pkg_resources
+
+root_attribs = {
+    '{http://www.w3.org/2001/XMLSchema-instance}schemaLocation':
+    'http://datacite.org/schema/kernel-4 '
+    'http://schema.datacite.org/meta/kernel-4.4/metadata.xsd',
+}
 
 del rules.rules['identifiers']
+
+validator.schema['definitions']['resourceTypeGeneral']['enum'] += [
+    "BookChapter",
+    "ComputationalNotebook",
+    "ConferencePaper",
+    "ConferenceProceeding",
+    "Dissertation",
+    "Journal",
+    "JournalArticle",
+    "OutputManagementPlan",
+    "PeerReview",
+    "Preprint",
+    "Report",
+    "Standard"
+]
+validator.schema['definitions']['relationType']['enum'].append("IsPublishedIn")
+
+def dump_etree(data):
+    """Convert JSON dictionary to DataCite v4.3 XML as ElementTree."""
+    return dump_etree_helper(data, rules, ns, root_attribs)
+
+
+def tostring(data, **kwargs):
+    """Convert JSON dictionary to DataCite v4.3 XML as string."""
+    return etree_to_string(dump_etree(data), **kwargs)
+
+
+def validate(data):
+    """Validate DataCite v4.3 JSON dictionary."""
+    return validator.is_valid(data)
+
 
 @rules.rule('identifiers')
 def identifiers(path, values):
