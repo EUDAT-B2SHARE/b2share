d26e35f43454990da87d136a78b3cba57a7d52ba
Giacomo Furlan <giacomo.furlan@csc.fi>
2024-02-21 14:28:24 +0200

    Update Homepage message for demo instances

--- webui/src/components/home.jsx
+++ webui/src/components/home.jsx
@@ -10,15 +10,28 @@ export const HomeRoute = React.createClass({
         const latestRecords = serverCache.getLatestRecords();
         const user = serverCache.getUser();
         const info = serverCache.getInfo();
+        const site_function = info.get('site_function');
         const b2access = info.get('b2access_registration_link');
         const training_site = info.get('training_site_link');
+        const divStyle = {
+            color: 'red',
+          };
         return (
             <div className="container-fluid home-page">
                 <div className="row">
                     <div className="col-sm-12">
                         <div style={{margin:'2em 0', textAlign: 'center'}}>
+                        { (site_function == "" || site_function == "production") ?
+                            <div>
                             <h3>Store and publish your research data</h3>
                             <p>Search in public datasets or register as a user to upload and publish your data!</p>
+                            </div>
+                            :
+                            <div>
+                            <h3 style={divStyle} >Attention: This is not a production instance. Please refrain from storing or publishing your research data here.</h3>
+                            <p>Service is intended solely for testing purposes, and data may be deleted at any time!</p>
+                            </div>
+                            }
                             { training_site ?
                                 <p>Please use <a href={training_site}>{training_site}</a> for testing or training.</p>
                                 : false }
